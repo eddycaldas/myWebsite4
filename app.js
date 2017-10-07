f0fe319630541759fc900d36ce019a1ff0eddd33
Eddy Caldas <eddycaldas@live.com>
2017-10-06 20:17:35 -0600

    first atte...

---
+++ app.js
@@ -0,0 +1,10 @@
+// function set_body_height()
+// {
+//     var wh = $(window).height();
+//     $('body').attr('style', 'height:' + wh + 'px;');
+// }
+// $(document).ready(function() {
+//     set_body_height();
+//     $(window).bind('resize', function() { set_body_height(); });
+// });
+
